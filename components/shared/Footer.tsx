1d583e30e2353eb1c3591ef491f79db4549ca814
Osama Sayed <osamasayedmuhammad@gmail.com>
2023-11-23 12:15:17 +0200

    edit container in tailwind configration

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,56 +1,58 @@
-import Image from "next/image";
-import Link from "next/link";
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Footer = () => {
   return (
-    <footer className="container mx-auto flex flex-col justify-between sm:flex-row mb-24">
-      <div className="font-medium">
-        <Image
-          src="/svg/footer-logo.svg"
-          width={158}
-          height={177}
-          alt="logo"
-          className="mb-6"
-        />
+    <footer className='container mx-auto mb-24 flex flex-col justify-between sm:flex-row'>
+      <div className='font-medium'>
+        <Link href='/'>
+          <Image
+            src='/svg/footer-logo.svg'
+            width={158}
+            height={177}
+            alt='logo'
+            className='mb-6'
+          />
+        </Link>
 
         <p>Via della Guastalla, 11-20122 Milano</p>
         <p>+39 3779522376</p>
         <p>info@Lucasquad.it</p>
       </div>
-      <div className="mt-5 flex gap-4 sm:mt-auto">
-        <Link href="/">
+      <div className='mt-5 flex gap-4 sm:mt-auto'>
+        <Link href='/'>
           <Image
-            src="/svg/social/facebook.svg"
+            src='/svg/social/facebook.svg'
             width={35}
             height={35}
-            alt="facebook icon"
+            alt='facebook icon'
           />
         </Link>
 
-        <Link href="/">
+        <Link href='/'>
           <Image
-            src={"/svg/social/instagram.svg"}
+            src={'/svg/social/instagram.svg'}
             width={35}
             height={35}
-            alt="logo"
+            alt='logo'
           />
         </Link>
 
-        <Link href="/">
+        <Link href='/'>
           <Image
-            src="/svg/social/twitter.svg"
+            src='/svg/social/twitter.svg'
             width={35}
             height={35}
-            alt="logo"
+            alt='logo'
           />
         </Link>
 
-        <Link href="/">
+        <Link href='/'>
           <Image
-            src="/svg/social/linkedin.svg"
+            src='/svg/social/linkedin.svg'
             width={35}
             height={35}
-            alt="logo"
+            alt='logo'
           />
         </Link>
       </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,38 +1,46 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      container: {
+        padding: {
+          DEFAULT: '1rem',
+          md:'2rem',
+          xl: '2.5rem',
+          '2xl': '4rem',
+        },
+      },
       screens: {
-        "2xs": "320px",
-        xs: "480px",
-        sm: "600px",
-        md: "768px",
-        lg: "1024px",
-        xl: "1280px",
-        "2xl": "1536px",
+        '2xs': '320px',
+        xs: '480px',
+        sm: '600px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px',
       },
       colors: {
         ls: {
-          primary: "#1A1A1A",
-          secondary: "#E5E3DF",
+          primary: '#1A1A1A',
+          secondary: '#E5E3DF',
         },
       },
       gridTemplateColumns: {
-        fill: "repeat(auto-fill,minmax(0,1fr))",
+        fill: 'repeat(auto-fill,minmax(0,1fr))',
       },
       transitionDelay: {
-        "1500": "1500ms",
-        "2000": "2000ms",
-        "2500": "2500ms",
+        '1500': '1500ms',
+        '2000': '2000ms',
+        '2500': '2500ms',
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require('tailwindcss-animate')],
 };
 export default config;
