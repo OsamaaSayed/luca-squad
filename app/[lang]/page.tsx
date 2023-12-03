7ae1c41d9e423300f7ff4f60a0a55242f4d33bdc
Samy Isaac <60799236+SamyIsaac1@users.noreply.github.com>
2023-12-03 13:34:35 +0200

    fix images

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -21,14 +21,14 @@ export default async function Home({
   return (
     <main>
       <section
-        style={{ backgroundImage: `url('./images/home/hero.jpg')` }}
+        style={{ backgroundImage: `url('/images/home/hero.jpg')` }}
         className='bg-main min-h-screen bg-cover bg-center bg-no-repeat'
       >
         <div className='flex min-h-screen items-center justify-center'>
           <div className='container mx-auto'>
             <div className='flex flex-col items-center justify-center gap-4 md:flex-row'>
               <Image
-                src='./svg/logo.svg'
+                src='/svg/logo.svg'
                 alt='logo'
                 width={110}
                 height={122}
@@ -36,21 +36,21 @@ export default async function Home({
                 className='delay-500 duration-500 ease-in-out animate-in fade-in zoom-in fill-mode-both'
               />
               <Image
-                src='./svg/v-line.svg'
+                src='/svg/v-line.svg'
                 alt='line'
                 width={2}
                 height={80}
                 className='hidden h-20 w-1 delay-1000 duration-500 ease-in-out animate-in fade-in-0 zoom-in-0 fill-mode-both md:inline-block'
               />
               <Image
-                src='./svg/luca.svg'
+                src='svg/luca.svg'
                 alt='luca'
                 width={260}
                 height={80}
                 className='delay-1500 duration-500 ease-in-out animate-in fade-in slide-in-from-left fill-mode-both'
               />
               <Image
-                src='./svg/squad.svg'
+                src='svg/squad.svg'
                 alt='squad'
                 width={325}
                 height={100}
