b1d983d1ccbe45e88a15eb423b874338e413f30f
Abdelhamid yehia <abdd.yahia@gmail.com>
2023-12-05 17:42:27 +0200

    btn responsive

--- app/[lang]/vacancies/components/discover.tsx
+++ app/[lang]/vacancies/components/discover.tsx
@@ -11,7 +11,7 @@ type DiscoverProps = {
 
 function Discover({ discover, lang }: DiscoverProps) {
   return (
-    <div className=' px-28 pb-20'>
+    <div className=' px-10 pb-20 md:px-28'>
       <Link
         href={`/${lang}/vacancies/list`}
         className='bg-main bg-ls-primary px-10 py-2 text-xl text-white'
