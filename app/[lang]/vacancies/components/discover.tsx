e5272d859cb93a65e53b44b49172a1abb8c612f8
Abdelhamid yehia <abdd.yahia@gmail.com>
2023-12-05 16:10:52 +0200

    change the white space bewten discover now btn and footer logo

--- app/[lang]/vacancies/components/discover.tsx
+++ app/[lang]/vacancies/components/discover.tsx
@@ -11,7 +11,7 @@ type DiscoverProps = {
 
 function Discover({ discover, lang }: DiscoverProps) {
   return (
-    <div className=' px-28 py-10'>
+    <div className=' px-28 pb-20'>
       <Link
         href={`/${lang}/vacancies/list`}
         className='bg-main bg-ls-primary px-10 py-2 text-xl text-white'
